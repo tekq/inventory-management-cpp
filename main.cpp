419e5d660bf09d000bd9ab63ad007e61d6aea0de
tekq <81379378+tekq@users.noreply.github.com>
2022-03-01 21:26:29 +0200

    added a help menu and made code look nicer
    CLion now screams less at me :)

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-string beggining_choice;
+string beginning_choice;
 string inv[100];
 int inv_occupied_slots = 0;
 bool scanned_inv_save = false;
@@ -15,7 +15,7 @@ void read_inv() {
     ifstream inv_save("inv.sav");
     while (i < 100) {
         inv_save >> inv[i];
-        if (inv[i] == "") {
+        if (inv[i].empty()) {
             i = 100;
         }
         inv_occupied_slots++;
@@ -51,7 +51,7 @@ void remove_item() {
     cin >> name;
 
     for (int i = 0; i < inv_occupied_slots; i++) {
-        if (inv[i] == name) {
+        if (inv[i] == name) { // remove items that are the same as the string "name"
             inv[i] = " ";
             break;
         }
@@ -77,32 +77,36 @@ void inv_menu() {
 }
 
 int main() {
-    if (scanned_inv_save == false) // necessary for inventory saving to work properly
+    if (!scanned_inv_save) // necessary for inventory saving to work properly
         read_inv();
 
     string name;
-    cout << "Simple Inventory\n";
-    cout << "Press 'i' to check inventory or 'c' to create an item.\n";
+    cout << "Simple Inventory Example\n";
+    cout << "Press 'h' for help.\n";
     cout << "Inventory items: " << inv_occupied_slots << "\n";
     cout << endl;
 
-    cin >> beggining_choice;
+    cin >> beginning_choice;
 
-    if (beggining_choice == "i") {
+    if (beginning_choice == "i" || beginning_choice == "I") { // show inventory
         inv_menu();
         main();
-    } else if (beggining_choice == "c") {
+    } else if (beginning_choice == "c" || beginning_choice == "C") { // add a new item to the inventory
         create_item();
         main();
-    } else if (beggining_choice == "s") {
+    } else if (beginning_choice == "s" || beginning_choice == "S") { // save the inventory to inv.sav
         save_inv();
-    } else if (beggining_choice == "q") {
-        return 0;
-    } else if (beggining_choice == "r") {
+    } else if (beginning_choice == "q" || beginning_choice == "Q") { // quit
+        return 256;
+    } else if (beginning_choice == "r" || beginning_choice == "R") { // read inventory again(might temporarily break the inventory)
         read_inv();
         main();
-    } else if (beggining_choice == "d") {
+    } else if (beginning_choice == "d" || beginning_choice == "D") { // trigger removal of items
         remove_item();
+        main();
+    } else if (beginning_choice == "h" || beginning_choice == "H") { // example help message
+        cout << "Example Help Message" << endl;
+        cout << " h - print this menu\n i - show current inventory\n c - create item\n s - save inventory to inv.sav\n q - quit\n r - read inventory(may cause breakage)\n d - remove item\n";
         main();
     } else {
         main();
